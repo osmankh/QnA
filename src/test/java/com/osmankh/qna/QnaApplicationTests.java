8be29f28c7284a753f77d3a7c99379684a759553
Osman KHODER <osman@merchantwise.com>
2021-04-23 16:12:00 +0300

    Added Spring boot project
    + Added the crafted Spring boot project by start.spring.io
    + Dependencies included in this project:
    	+ Spring Web
    	+ PostgreSQL Driver
    	+ Spring Data JPA
    	+ Flyway Migration

---
+++ src/test/java/com/osmankh/qna/QnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmankh.qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
